12692e94645d46812a97005fa1ee7bfb8dec0274
Cristrian <cctrianag@unal.edu.co>
2021-10-21 13:56:36 -0500

    Pipenv added and layouts created

---
+++ app.py
@@ -0,0 +1,10 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+#Local Imports
+import callbacks
+from layouts import board_layout
+from layouts import dash_imperiums_components as dic
+from data import data
+
---
+++ layouts/board_layout.py
@@ -0,0 +1,52 @@
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+#Local Imports
+from layouts import dash_imperiums_components as dic
+from data import data
+
+def gen_layout(appname):
+
+    
+    #Components,Here You will create the components that are
+    #going to be included in the layout.
+
+    example_component = [
+        dbc.Row([
+            dbc.Col(dbc.Card('This is a card in column 1')),
+            dbc.Col(dbc.Card('This is a card in column 2')),
+        ])
+    ]
+
+    #graphics, here are the graphics 
+
+    graphics = html.Div(id=f'{appname}-graphics')
+
+    #General layout
+
+    layout = dbc.Container(
+        [
+            html.Div(example_component),
+            html.Div(graphics)
+        ]
+
+    )
+    return layout
+
+def gen_graphics_layout(page: int, appname: str):
+    """Generates graphics layout.
+    Based on the dashboard page it generates the graphics layout
+    
+    Args:
+        page: The number of the page.
+        appname: the name of the app for the id's
+
+    Returns:
+        A list containing the graphics, ready to be pass as a children 
+        of a html component.
+    """
+    if page == 1:
+        graphics = []
+    elif page ==2: 
+        graphics = []
+    return graphics
