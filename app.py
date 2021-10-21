e4c6819b5838ac9395dc4176f7debe393c836582
Cristrian <cctrianag@unal.edu.co>
2021-10-21 14:28:50 -0500

    Update: This is a template to create a dashboard We use dash plotly

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
+from dash.html.Col import Col
 import dash_bootstrap_components as dbc
 
 #Local Imports
@@ -8,3 +9,40 @@ from layouts import board_layout
 from layouts import dash_imperiums_components as dic
 from data import data
 
+
+app = Dash(__name__, suppress_callback_exceptions=True)
+
+#Here we generate both, live and picture layouts
+
+#number of pages for the pagination
+n_pages = 3
+
+#Layouts definition
+live_layout = [
+    html.Div(board_layout.gen_layout('live')),
+    html.Div(dic.gen_download_button('live-down-btn', 'live-download-dataframe')),
+    html.Div(dbc.Pagination(id='live-pagination', max_value=n_pages, active_page=1))
+    ]
+
+picture_layout = [
+    html.Div(board_layout.gen_layout('pic')),
+    html.Div(dic.gen_download_button('pic-down-btn', 'pic-download-dataframe')),
+    html.Div(dbc.Pagination(id='pic-pagination', max_value=n_pages, active_page=1))
+    ]
+
+app.layout = html.Div([
+    dbc.Row(
+        dbc.Col(html.H1('Nombre Del Tablero'))
+    ),
+    dcc.Tabs(
+        [dcc.Tab(label='En vivo', value='live-tab'),
+         dcc.Tab(label='Fotografía', value='pic-tab')
+        ]
+    )
+])
+
+#Down Here you will import and create the callbacks
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
---
+++ data/data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def get_dataframe():
+    df = pd.read_csv('data/example.csv')
+    return df
